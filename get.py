da4538887bdee18164cf3fdf5cfaffad7c11b677
authmillenon <authmill@spline.de>
2012-05-09 16:35:39 +0200

    Establish server connection

--- get.py
+++ get.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+
+import imaplib
+
+if __name__ == '__main__':
+    host = '<host>'
+    port = '<port>'
+    username = '<username>'
+    password = '<password>'
+    mailbox = '<mailbox>'
+
+    server = imaplib.IMAP4_SSL(host, port)
+    server.login(username, password)
+
+    server.close()
+    server.logout()
+
