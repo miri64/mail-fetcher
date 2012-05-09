5dc25327078c1b3eca0bb97e2553ff1d63d5af68
authmillenon <authmill@spline.de>
2012-05-09 17:52:39 +0200

    Fetch emails

--- get.py
+++ get.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import imaplib
+import sys, imaplib, getpass
 
 if __name__ == '__main__':
     host = '<host>'
@@ -9,9 +9,23 @@ if __name__ == '__main__':
     password = '<password>'
     mailbox = '<mailbox>'
 
-    server = imaplib.IMAP4_SSL(host, port)
-    server.login(username, password)
-
-    server.close()
-    server.logout()
+    try:
+        server = imaplib.IMAP4_SSL(host, port)
+        server.login(username, password)
+        result, data = server.select(mailbox)
+        try:
+            if result != 'OK':
+                raise imaplib.IMAP4.error("Error finding mailbox '%s'." % mailbox)
+            result, data = server.uid("search", None, "ALL")
+            uids = data[0].split()
+            raw_mails = []
+            for uid in [uids[0]]:
+                result, data = server.uid("fetch", uid, "(RFC822)")
+                raw_mails.append(data[0][1])
+        finally:
+            server.close()
+    except imaplib.IMAP4.error as e:
+        print(e, file=sys.stderr)
+    finally:
+        server.logout()
 
